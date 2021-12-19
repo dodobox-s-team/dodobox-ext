27547bd57210dc920bec51dcc4bc2eb9ee004638
Mathieu Walravens <m.walravens@students.ephec.be>
2021-12-19 16:10:54 +0100

    Setup database

--- api/app.py
+++ api/app.py
@@ -1,9 +1,11 @@
 from fastapi import Depends, FastAPI
+import asyncio
 from fastapi.security import OAuth2PasswordRequestForm
 from jose import jwt
 
 from api.auth import ALGORITHM, SECRET_KEY, TokenModel
 from api.routers import acmerelay
+from api.models.base import db
 from api.routers.acmerelay.request import OVHRequest
 
 app = FastAPI(title="Dodobox External API")
@@ -11,7 +13,20 @@ app.include_router(acmerelay.router)
 
 
 @app.on_event("startup")
-def startup():
+async def startup():
+    exception = None
+    for retries in range(5):
+        try:
+            await db.connect()
+            break
+        except ConnectionRefusedError as e:
+            exception = e
+            backoff = 2 ** retries / 10
+            print(f"Couldn't connect to the database. Retrying in {backoff}s.")
+            await asyncio.sleep(backoff)
+    else:
+        raise exception
+
     # Check for ENV variables. If one is missing, prevent from starting.
     if SECRET_KEY is None:
         raise ValueError("'SECRET_KEY' env variable is not set. Use `os.urandom(32)` to create a strong key.")
@@ -20,6 +35,11 @@ def startup():
         key = f"OVH_{key}"
         if getattr(OVHRequest, key) is None:
             raise ValueError(f'{key!r} env variable is required but not set.')
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    await db.disconnect()
 
 
 @app.post("/token", tags=["security"], response_model=TokenModel)
